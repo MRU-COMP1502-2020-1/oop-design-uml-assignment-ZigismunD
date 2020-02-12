7952ff91103a06f63238f5703860f621f7e3f3b2
Sami Sikkilä <sami.sikkila@metropolia.fi>
2020-02-11 21:12:33 -0700

    Add files via upload
    Completed lab

--- coupling&cohesion/prog3.java
+++ coupling&cohesion/prog3.java
@@ -1,31 +1,47 @@
 class MyReader {
     // -------------- unrelated functions
-    public boolean validateLocation(String pathIP) {
-         return ping(pathIP) && checkFTP(pathIP);
+    private boolean validateLocation(String pathIP) {
+        return ping(pathIP) && checkFTP(pathIP);
     }
     private boolean checkFTP(String pathIP) {
-         return true;
+        return true;
     }
 
     private boolean ping(String pathIP) {
-          return true;
+        return true;
     }
 
     // -------------- functions related to read resource
 
     // read the resource from disk
-    public String readFromDisk(String fileName) {
-         return "data of " + fileName;
+    public String readFromDisk(String pathIP, String fileName) {
+        if (validateLocation(pathIP)) {
+            return "data of " + fileName;
+        } else {
+            System.out.println("Invalid location");
+            return null;
+        }
+
     }
 
     // read the resource from web
-    public String readFromWeb(String url) {
-         return "data of " + url;
+    public String readFromWeb(String pathIP,String url) {
+        if (validateLocation(pathIP)) {
+            return "data of " + url;
+        } else {
+            System.out.println("Invalid location");
+            return null;
+        }
     }
 
     // read the resource from network
-    public String readFromNetwork(String networkAddress) {
-         return "data of " + networkAddress;
+    public String readFromNetwork(String pathIP,String networkAddress) {
+        if (validateLocation(pathIP)) {
+            return "data of " + networkAddress;
+        } else {
+            System.out.println("Invalid location");
+            return null;
+        }
     }
 
 }
